6372e81330ed7dd6bd6152a54ff758f2147b5761
Kamil Riyas <s.kamilriyas@gmail.com>
2025-01-01 18:24:33 +0530

    add sk-console project with initial implementation and configuration

---
+++ sk-console/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder();
+
+var uri = new Uri("http://localhost:11434");
+
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+builder.Services.AddOllamaChatCompletion("llama3.2", uri);
+#pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+var kernel = builder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+try
+{
+    ChatMessageContent chatMessage = await chatCompletionService.GetChatMessageContentAsync("Hi, can you tell me a dad joke");
+    Console.WriteLine(chatMessage.ToString());
+}
+catch (Exception ex)
+{
+    
+    System.Console.WriteLine(ex.Message);
+}
+
+
