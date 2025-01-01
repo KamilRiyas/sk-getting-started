ec8799aff81ceb7edcd020f9441fd2eaaa49626b
Kamil Riyas <s.kamilriyas@gmail.com>
2025-01-01 18:41:52 +0530

    refactor Program.cs for improved readability and formatting

--- sk-console/Program.cs
+++ sk-console/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.SemanticKernel.ChatCompletion;
 
 var builder = Kernel.CreateBuilder();
-
 var uri = new Uri("http://localhost:11434");
 
 #pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
@@ -10,17 +9,16 @@ builder.Services.AddOllamaChatCompletion("llama3.2", uri);
 #pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 var kernel = builder.Build();
-
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
 try
 {
-    ChatMessageContent chatMessage = await chatCompletionService.GetChatMessageContentAsync("Hi, can you tell me a dad joke");
+    ChatMessageContent chatMessage = await chatCompletionService
+                                    .GetChatMessageContentAsync("Hi, can you tell me a dad joke");
     Console.WriteLine(chatMessage.ToString());
 }
 catch (Exception ex)
 {
-    
     System.Console.WriteLine(ex.Message);
 }
 
